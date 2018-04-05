7b5a12721ac7927c4dcfcf5854f8978384f26fce
trungnd <nguyenductrung95@gmail.com>
2018-04-05 21:16:34 +0700

    gitignore

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -1,11 +1,14 @@
-import os, cv2, numpy as np, matplotlib.pyplot as plt
+import os, cv2, numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import struct
 from shutil import copyfile
 
-video_path = '/media/data/datasets/Kinect2017-10/Datasets/'
-out_path = './data/'
-gt_root = '/media/data/datasets/Kinect_vis_annotate/v5/Kinect3/'
-
+# video_path = '/media/data/datasets/Kinect2017-10/Datasets/'
+video_path = '/Users/trungnd/pfiev/yolo-trainer/video/'
+out_path = '/Users/trungnd/pfiev/yolo-trainer/output/'
+gt_root = '/Users/trungnd/pfiev/yolo-trainer/data/'
 
 def read_depth(file_reader):
     try:
@@ -29,12 +32,15 @@ def read_depth(file_reader):
 
         # depth=cv2
 
-        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        # print mask.shape
+        # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if(len(contours) > 0):
+            cnt = contours[0]
+            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
         return mask
     except Exception as e:
         print
-        'Error: ' + str(e)
+        'Error read depth: ' + str(e)
         return None
 
 
@@ -44,22 +50,25 @@ for _, _, files in os.walk(gt_root):
     break
 
 for path in gt_files:
-    # print path
+    print path
+    print(gt_root+path)
     f = open(gt_root + path, 'rt')
     lines = f.read().strip().split('\n')
     file_name = path.split('.')[0]
+    print file_name
 
     video_dir = out_path + file_name
-    # print video_dir
+    print video_dir
+
     # if not os.path.exists(video_dir):
     # 	os.makedirs(video_dir)
     # else:
     # 	continue
 
     fourcc = cv2.VideoWriter_fourcc(*'H264')
-    out = cv2.VideoWriter('./data/' + file_name + '_mask.mp4', fourcc, 20.0, (640, 480))
-
-    f_d = open(video_path + file_name + '/Kinect_3/depth.bin', 'rb')
+    out = cv2.VideoWriter(out_path + file_name + '_mask.mp4', fourcc, 20.0, (640, 480))
+    print video_path + 'Kinect_3/depth.bin'
+    f_d = open(gt_root + path, 'rb')
 
     frame_num = 0
     # copyfile(video_path+file_name+'/Kinect_3/color.avi', './data/'+file_name+'.avi')
@@ -71,6 +80,9 @@ for path in gt_files:
         # plt.imshow(img)
         # plt.show()
         frame_num += 1
+        print("Frame: ", frame_num)
+        if frame_num > 1000:
+            break
         if img is None:
             break
 
@@ -81,4 +93,3 @@ for path in gt_files:
 
 # print lines
 # break
-
